a16cc70cc836050003ffce66ed00c2ec3533f373
Cullen <cullenjcasey@gmail.com>
2023-03-04 20:17:58 -0800

    changed strategy

--- assets/beast_list.js
+++ assets/beast_list.js
@@ -1,265 +1,95 @@
-const beastList = [
-        {
-            "name": "Baboon"
-        },
-        {
-            "name": "Bat"
-        },
-        {
-            "name": "Black Bear"
-        },
-        {
-            "name": "Blood Hawk"
-        },
-        {
-            "name": "Cat"
-        },
-        {
-            "name": "Crab"
-        },
-        {
-            "name": "Ape"
-        },
-        {
-            "name": "Axe Beak"
-        },
-        {
-            "name": "Boar"
-        },
-        {
-            "name": "Camel"
-        },
-        {
-            "name": "Constrictor Snake"
-        },
-        {
-            "name": "Crocodile"
-        },
-        {
-            "name": "Badger"
-        },
-        {
-            "name": "Brown Bear"
-        },
-        {
-            "name": "Deer"
-        },
-        {
-            "name": "Giant Centipede"
-        },
-        {
-            "name": "Giant Fire Beetle"
-        },
-        {
-            "name": "Giant Hyena"
-        },
-        {
-            "name": "Giant Octopus"
-        },
-        {
-            "name": "Giant Rat (Diseased)"
-        },
-        {
-            "name": "Giant Spider"
-        },
-        {
-            "name": "Giant Weasel"
-        },
-        {
-            "name": "Hunter Shark"
-        },
-        {
-            "name": "Hyena"
-        },
-        {
-            "name": "Lion"
-        },
-        {
-            "name": "Dire Wolf"
-        },
-        {
-            "name": "Eagle"
-        },
-        {
-            "name": "Flying Snake"
-        },
-        {
-            "name": "Giant Bat"
-        },
-        {
-            "name": "Giant Constrictor Snake"
-        },
-        {
-            "name": "Giant Scorpion"
-        },
-        {
-            "name": "Giant Sea Horse"
-        },
-        {
-            "name": "Giant Toad"
-        },
-        {
-            "name": "Giant Wolf Spider"
-        },
-        {
-            "name": "Goat"
-        },
-        {
-            "name": "Draft Horse"
-        },
-        {
-            "name": "Elephant"
-        },
-        {
-            "name": "Elk"
-        },
-        {
-            "name": "Giant Badger"
-        },
-        {
-            "name": "Giant Crab"
-        },
-        {
-            "name": "Giant Crocodile"
-        },
-        {
-            "name": "Giant Goat"
-        },
-        {
-            "name": "Giant Lizard"
-        },
-        {
-            "name": "Giant Poisonous Snake"
-        },
-        {
-            "name": "Giant Wasp"
-        },
-        {
-            "name": "Killer Whale"
-        },
-        {
-            "name": "Lizard"
-        },
-        {
-            "name": "Frog"
-        },
-        {
-            "name": "Giant Ape"
-        },
-        {
-            "name": "Giant Boar"
-        },
-        {
-            "name": "Giant Eagle"
-        },
-        {
-            "name": "Giant Elk"
-        },
-        {
-            "name": "Giant Frog"
-        },
-        {
-            "name": "Giant Owl"
-        },
-        {
-            "name": "Giant Rat"
-        },
-        {
-            "name": "Giant Shark"
-        },
-        {
-            "name": "Giant Vulture"
-        },
-        {
-            "name": "Hawk"
-        },
-        {
-            "name": "Jackal"
-        },
-        {
-            "name": "Mammoth"
-        },
-        {
-            "name": "Mastiff"
-        },
-        {
-            "name": "Plesiosaurus"
-        },
-        {
-            "name": "Poisonous Snake"
-        },
-        {
-            "name": "Rat"
-        },
-        {
-            "name": "Raven"
-        },
-        {
-            "name": "Tyrannosaurus Rex"
-        },
-        {
-            "name": "Vulture"
-        },
-        {
-            "name": "Panther"
-        },
-        {
-            "name": "Quipper"
-        },
-        {
-            "name": "Reef Shark"
-        },
-        {
-            "name": "Rhinoceros"
-        },
-        {
-            "name": "Saber-Toothed Tiger"
-        },
-        {
-            "name": "Warhorse"
-        },
-        {
-            "name": "Octopus"
-        },
-        {
-            "name": "Owl"
-        },
-        {
-            "name": "Scorpion"
-        },
-        {
-            "name": "Spider"
-        },
-        {
-            "name": "Stirge"
-        },
-        {
-            "name": "Triceratops"
-        },
-        {
-            "name": "Weasel"
-        },
-        {
-            "name": "Mule"
-        },
-        {
-            "name": "Polar Bear"
-        },
-        {
-            "name": "Pony"
-        },
-        {
-            "name": "Riding Horse"
-        },
-        {
-            "name": "Sea Horse"
-        },
-        {
-            "name": "Tiger"
-        },
-        {
-            "name": "Wolf"
-        }
-    
+// const beastList = [
+//     "Baboon",
+//     "Bat",
+//     "Black Bear",
+//     "Blood Hawk",
+//     "Cat",
+//     "Crab",
+//     "Ape",
+//     "Axe Beak",
+//     "Boar",
+//     "Camel",
+//     "Constrictor Snake",
+//     "Crocodile",
+//     "Badger",
+//     "Brown Bear",
+//     "Deer",
+//     "Giant Centipede",
+//     "Giant Fire Beetle",
+//     "Giant Hyena",
+//     "Giant Octopus",
+//     "Giant Rat (Diseased)",
+//     "Giant Spider",
+//     "Giant Weasel",
+//     "Hunter Shark",
+//     "Hyena",
+//     "Lion",
+//     "Dire Wolf",
+//     "Eagle",
+//     "Flying Snake",
+//     "Giant Bat",
+//     "Giant Constrictor Snake",
+//     "Giant Scorpion",
+//     "Giant Sea Horse",
+//     "Giant Toad",
+//     "Giant Wolf Spider",
+//     "Goat",
+//     "Draft Horse",
+//     "Elephant",
+//     "Elk",
+//     "Giant Badger",
+//     "Giant Crab",
+//     "Giant Crocodile",
+//     "Giant Goat",
+//     "Giant Lizard",
+//     "Giant Poisonous Snake",
+//     "Giant Wasp",
+//     "Killer Whale",
+//     "Lizard",
+//     "Frog",
+//     "Giant Ape",
+//     "Giant Boar",
+//     "Giant Eagle",
+//     "Giant Elk",
+//     "Giant Frog",
+//     "Giant Owl",
+//     "Giant Rat",
+//     "Giant Shark",
+//     "Giant Vulture",
+//     "Hawk",
+//     "Jackal",
+//     "Mammoth",
+//     "Mastiff",
+//     "Plesiosaurus",
+//     "Poisonous Snake",
+//     "Rat",
+//     "Raven",
+//     "Tyrannosaurus Rex",
+//     "Vulture",
+//     "Panther",
+//     "Quipper",
+//     "Reef Shark",
+//     "Rhinoceros",
+//     "Saber-Toothed Tiger",
+//     "Warhorse",
+//     "Octopus",
+//     "Owl",
+//     "Scorpion",
+//     "Spider",
+//     "Stirge",
+//     "Triceratops",
+//     "Weasel",
+//     "Mule",
+//     "Polar Bear",
+//     "Pony",
+//     "Riding Horse",
+//     "Sea Horse",
+//     "Tiger",
+//     "Wolf"
+// ]
 
+monsters [
+    "Baboon", "Bat", "Black Bear", "Blood Hawk", "Cat", "Crab", "Ape", "Axe Beak", "Boar", "Camel", "Constrictor Snake", "Crocodile", "Badger", "Brown Bear", "Deer", "Giant Centipede", "Giant Fire Beetle", "Giant Hyena", "Giant Octopus", "Giant Rat (Diseased)", "Giant Spider", "Giant Weasel", "Hunter Shark", "Hyena", "Lion", "Dire Wolf", "Eagle", "Flying Snake", "Giant Bat", "Giant Constrictor Snake", "Giant Scorpion", "Giant Sea Horse", "Giant Toad", "Giant Wolf Spider", "Goat", "Draft Horse", "Elephant", "Elk", "Giant Badger", "Giant Crab", "Giant Crocodile", "Giant Goat", "Giant Lizard", "Giant Poisonous Snake", "Giant Wasp", "Killer Whale", "Lizard", "Frog", "Giant Ape", "Giant Boar", "Giant Eagle", "Giant Elk", "Giant Frog", "Giant Owl", "Giant Rat", "Giant Shark", "Giant Vulture", "Hawk", "Jackal", "Mammoth", "Mastiff", "Plesiosaurus", "Poisonous Snake", "Rat", "Raven", "Tyrannosaurus Rex", "Vulture", "Panther", "Quipper", "Reef Shark", "Rhinoceros", "Saber-Toothed Tiger", "Warhorse", "Octopus", "Owl", "Scorpion", "Spider", "Stirge", "Triceratops", "Weasel", "Mule", "Polar Bear", "Pony", "Riding Horse", "Sea Horse", "Tiger", "Wolf"
 ]
+
+export { beastList };
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,50 @@
-let getAnimal = function() {
-    fetch("https://www.dnd5eapi.co/api/monsters/brown-bear")
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data.type);
-            });
-        });
+// const beastList = [
+//     "Baboon", "Bat", "Black Bear", "Blood Hawk", "Cat", "Crab", "Ape", "Axe Beak", "Boar", "Camel", "Constrictor Snake", "Crocodile", "Badger", "Brown Bear", "Deer", "Giant Centipede", "Giant Fire Beetle", "Giant Hyena", "Giant Octopus", "Giant Rat (Diseased)", "Giant Spider", "Giant Weasel", "Hunter Shark", "Hyena", "Lion", "Dire Wolf", "Eagle", "Flying Snake", "Giant Bat", "Giant Constrictor Snake", "Giant Scorpion", "Giant Sea Horse", "Giant Toad", "Giant Wolf Spider", "Goat", "Draft Horse", "Elephant", "Elk", "Giant Badger", "Giant Crab", "Giant Crocodile", "Giant Goat", "Giant Lizard", "Giant Poisonous Snake", "Giant Wasp", "Killer Whale", "Lizard", "Frog", "Giant Ape", "Giant Boar", "Giant Eagle", "Giant Elk", "Giant Frog", "Giant Owl", "Giant Rat", "Giant Shark", "Giant Vulture", "Hawk", "Jackal", "Mammoth", "Mastiff", "Plesiosaurus", "Poisonous Snake", "Rat", "Raven", "Tyrannosaurus Rex", "Vulture", "Panther", "Quipper", "Reef Shark", "Rhinoceros", "Saber-Toothed Tiger", "Warhorse", "Octopus", "Owl", "Scorpion", "Spider", "Stirge", "Triceratops", "Weasel", "Mule", "Polar Bear", "Pony", "Riding Horse", "Sea Horse", "Tiger", "Wolf"
+// ]
+
+const beastList = [
+    "Baboon", "Bat", "Black Bear", "Blood Hawk", "Cat", "Crab", "Ape", "Axe Beak", "Boar", "Camel", "Constrictor Snake", "Crocodile", "Badger", "Brown Bear", "Deer", "Giant Centipede", "Giant Fire Beetle", "Giant Hyena", "Giant Octopus", "Giant Rat (Diseased)", "Giant Spider", "Giant Weasel", "Hunter Shark", "Hyena", "Lion", "Dire Wolf", "Eagle", "Flying Snake", "Giant Bat", "Giant Constrictor Snake", "Giant Scorpion", "Giant Sea Horse", "Giant Toad", "Giant Wolf Spider", "Goat", "Draft Horse", "Elephant", "Elk", "Giant Badger", "Giant Crab", "Giant Crocodile", "Giant Goat", "Giant Lizard", "Giant Poisonous Snake", "Giant Wasp", "Killer Whale", "Lizard", "Frog", "Giant Ape", "Giant Boar", "Giant Eagle", "Giant Elk", "Giant Frog", "Giant Owl", "Giant Rat", "Giant Shark", "Giant Vulture", "Hawk", "Jackal", "Mammoth", "Mastiff", "Plesiosaurus", "Poisonous Snake", "Rat", "Raven", "Tyrannosaurus Rex", "Vulture", "Panther", "Quipper", "Reef Shark", "Rhinoceros", "Saber-Toothed Tiger", "Warhorse", "Octopus", "Owl", "Scorpion", "Spider", "Stirge", "Triceratops", "Weasel", "Mule", "Polar Bear", "Pony", "Riding Horse", "Sea Horse", "Tiger", "Wolf"
+]
+
+
+// // import { beastList } from "./beast_list";
+
+// let getAnimal = function() {
+//     fetch("https://www.dnd5eapi.co/api/monsters/brown-bear")
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 console.log(data.type);
+//             });
+//         });
+// };
+
+// let makeAnimalList = function() {
+//     let animalList = [];
+//     let monsterList = [];
+//     // fetch("https://www.dnd5eapi.co/api/monsters")
+//     fetch ("https://www.dnd5eapi.co/api/monsters?challenge_rating=beast")
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 console.log(data)
+//             })
+//         })
+// };
+
+let displayBeastList = function() {
+    // console.log(beastList[5])
+    console.log(beastList.length)
+    console.log(typeof beastList)
+    let currentBeastList = document.getElementById("beast-list");
+    // currentBeastList.textContent = beastList;
+    for (let i = 0; i < beastList.length; i++) {
+        console.log("test");
+        console.log(i);
+        console.log(beastList[i]);
+        currentBeastList.push(beastList[i]);
+        
+    }
 };
 
-let makeAnimalList = function() {
-    let animalList = [];
-    let monsterList = [];
-    // fetch("https://www.dnd5eapi.co/api/monsters")
-    fetch ("https://www.dnd5eapi.co/api/monsters?challenge_rating=beast")
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data)
-            })
-        })
-};
-
-let beastList = [];
-
-let pullList = function() {
-    
-}
-
+displayBeastList();
 // getAnimal();
-makeAnimalList();
+// makeAnimalList();
